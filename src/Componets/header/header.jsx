65ad68dff62c178659eda813588cc83104c45487
Bini Devassy <binidevassy@outlook.com>
2023-09-07 12:02:49 +0200

    Necessary changes

--- src/Componets/header/header.jsx
+++ src/Componets/header/header.jsx
@@ -8,11 +8,14 @@ const header = () => {
   return (
     <header>
       <div className="container header_container">
+        
         <h5>Hello I'm</h5>
         <h1>BINI DEVASSY</h1>
         <h5 className="text-light">Frontend Developer </h5>
+        
         <CTA/>
         <HeaderSocials/>
+        
 
 
 
