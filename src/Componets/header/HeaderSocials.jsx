6d688ead5edcf26eec92d4ba86ee66c05f80bb7a
Bini Devassy <binidevassy@outlook.com>
2023-09-07 17:14:45 +0200

    Linkdin link changed

--- src/Componets/header/HeaderSocials.jsx
+++ src/Componets/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ const HeaderSocials = () => {
   return (
     <div className="header_socials">
       <div className="headersocial__icon">
-        <a href="www.linkedin.com/in/bini-devassy-89b8a727a" target="blank"><ImLinkedin/></a>
+        <a href="https://www.linkedin.com/in/bini-devassy-89b8a727a/" target="blank"><ImLinkedin/></a>
         </div>
         <a href="https://github.com/Bini-Devassy" target="blank"><BsGithub/> </a>
         <a href="http://dribble.com" target="blank"><FaDribbbleSquare/> </a>
